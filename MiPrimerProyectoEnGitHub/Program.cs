0318870e114b9e5a389ff728689a259d7dbdd303
Andrés Panitsch <andrespanitsch@hotmail.com>
2019-03-03 11:32:51 -0300

    Mi primer commit

--- MiPrimerProyectoEnGitHub/Program.cs
+++ MiPrimerProyectoEnGitHub/Program.cs
@@ -10,7 +10,7 @@ namespace MiPrimerProyectoEnGitHub
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola mundo!");
+            Console.WriteLine("Hola mundo!!");
         }
     }
 }
